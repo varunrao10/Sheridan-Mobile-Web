628e33a599e312620e74a89ff4016979835da248
AutonomousProgrammer <32847648+AutonomousProgrammer@users.noreply.github.com>
2019-03-26 18:48:19 -0400

    save

--- Assignments/Angular/raova-A3/src/app/mySongs.ts
+++ Assignments/Angular/raova-A3/src/app/mySongs.ts
@@ -1,10 +1,9 @@
-import { songInfo} from './songInfo';
+import { songInfo} from "./songInfo";
 
 export const MYSONGS: songInfo[] = 
-[{
-    songName:'Clocks', songArtist:'Coldplay', songGenre:'Brit Pop', songReleaseYear:'2009',songPic:'d'
-   songName:'Speed Of Sound', songArtist:'Coldplay', songGenre:'Brit Pop', songReleaseYear:'2005',songPic:'d'
-    songName:'Linger', songArtist:'Cranberries', songGenre:'Alternative/Indie', songReleaseYear:'1993',songPic:'d'
-    songName:'Zombie', songArtist:'Cranberries', songGenre:'Alternative/Indie', songReleaseYear:'1993',songPic:'d'
-    
-}];    
+[
+    {"songName":"Clocks", "songArtist":"Coldplay", "songGenre":"Brit Pop", "songReleaseYear":"2009","songPic":"d"},
+    {"songName":"Speed Of Sound", "songArtist":"Coldplay", "songGenre":"Brit Pop", "songReleaseYear":"2005","songPic":"d"},
+    {"songName":"Linger", "songArtist":"Cranberries", "songGenre":"Alternative/Indie", "songReleaseYear":"1993","songPic":"d"},
+    {"songName":"Zombie", "songArtist":"Cranberries", "songGenre":"Alternative/Indie", "songReleaseYear":"1993","songPic":"d"}
+];
